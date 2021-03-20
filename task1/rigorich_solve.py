5618edff8fe12786b19a856f79096a681ebd9858
Rigorich <nikita.grishaev@mail.ru>
2021-03-20 21:47:38 +0300

    Rigorich solve

---
+++ task1/rigorich_solve.py
@@ -0,0 +1,71 @@
+print('1) Text statistics\n')
+
+import re
+import string
+
+def TextFromFile():
+    DELIMETER = '\\'
+    TEXTFILE = 'text.txt'
+    path = __file__.replace(__file__[__file__.rfind(DELIMETER):], DELIMETER) + TEXTFILE
+    file = open(path, 'r') 
+    text = file.read()
+    file.close()
+    return text
+ 
+text = TextFromFile()
+
+print(text)
+print("\n\n-----")
+
+
+def FrequencyList(s: str) -> list:
+    cnt = {}
+    for i in re.sub('[^\w\s]', ' ', s.lower().replace('\n', ' ')).split(' '):
+        if (len(i) > 0):
+            cnt.setdefault(i, 0)
+            cnt[i] += 1
+    return sorted(cnt.items(), key = lambda item: item[1], reverse = True)
+
+print("Frequency list:")
+print(FrequencyList(text))
+print("-----")
+
+
+def WordsCounts(s: str) -> list:
+    cnt = []
+    for p in re.sub('[^\w\s.]', '', re.sub('[!?]', '.', s)).split('.'):
+        words = 0
+        for i in p.split(' '):
+            if (len(i) > 0):
+                words += 1
+        if (words > 0):
+            cnt += [words]
+    return cnt
+
+def Average(l: list):
+    return sum(l) / len(l)
+
+def Median(l: list):
+    l = sorted(l)
+    return (l[len(l) // 2] + l[(len(l) - 1) // 2]) / 2
+
+print("Average amount of words in sentence:", Average(WordsCounts(text)))
+print("Median amount of words in sentence:", Median(WordsCounts(text)))
+print("-----")
+
+
+def Ngramms(s: str, K: int) -> str:
+    cnt = {}
+    s = re.sub('[^a-z]', '', s.lower())
+    for i in range(len(s) - K + 1):
+        subs = s[i:i+K]
+        cnt.setdefault(subs, 0)
+        cnt[subs] += 1
+    return sorted(cnt.items(), key = lambda item: item[1], reverse = True)
+
+K = 4
+N = 10
+dct = Ngramms(text, K)
+print(f"Top-{N} {K}-gramms:")
+print([dct[i] for i in range(min(N, len(dct)))])
+print("-----")
