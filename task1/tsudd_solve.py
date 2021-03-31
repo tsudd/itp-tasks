fed6207c54fc26a50c4dbae00138548bce6ab38c
Rigorich <nikita.grishaev@mail.ru>
2021-03-31 20:39:43 +0300

    git reset fix

--- task1/tsudd_solve.py
+++ task1/tsudd_solve.py
@@ -1,4 +1,4 @@
-# first solution
+import re
 
 TEXT_EXAMPLE = "By some measures, Joe Biden has gotten off to a slow start as President. His transition was stalled by " \
               "Donald Trump’s refusal to acknowledge defeat. His Cabinet nominees are taking longer than most to be " \
@@ -34,21 +34,88 @@ TEXT_EXAMPLE = "By some measures, Joe Biden has gotten off to a slow start as Pr
               "a bold piece of legislation,\" says Delaware Sen. Chris Coons, a Biden protégé who occupies the " \
               "President\'s former Senate seat. “He\'s continuing his outreach to Republicans."
 
+SYNTAX_SYMBOLS_REGEX = '[^\w\s]'
+SYNTAX_SYMBOLS_AND_DOT_REGEX = '[^\w\s.]'
+END_SYMBOLS_REGEX = '[!?]'
 
-def process_text(text):
+FILE_OUTPUT = True
+OUTPUT_FILE_NAME = "dist.txt"
+OUTPUT_TOPIC = "Text processing\n"
+TABLE_TOPIC = "Word ==> count\n"
+
+
+def process_text(text, fp=None):
     assert type(text) == str
 
-    statements = text.lover().split('.')
+    words = re.sub(SYNTAX_SYMBOLS_REGEX, ' ', text.lower().replace('\n', ' ')).split(' ')
     words_stat = {}
-    statement_words_count = 0
+    words_count = 0
+    statement_words = []
 
-    for statement in statements:
-        words = statement.replace()
+    for word in words:
+        if len(word) == 0:
+            continue
+        words_count += 1
+        try:
+            words_stat[word] += 1
+        except KeyError:
+            words_stat[word] = 1
+    for sentence in re.sub(SYNTAX_SYMBOLS_AND_DOT_REGEX, ' ', re.sub(END_SYMBOLS_REGEX, '.', text)).split('.'):
+        count = 0
+        for word in sentence.split(' '):
+            if len(word) > 0:
+                count += 1
+        if count > 0:
+            statement_words.append(count)
 
+    print_stats(words_stat, fp)
+    average_words_amount(statement_words, fp)
+    median_word_count(statement_words, fp)
+
+
+def print_stats(words_stat, fp=None):
+    assert type(words_stat) == dict
+
+    sorted(words_stat.items(), key=lambda item: item[1], reverse=True)
+    if fp:
+        fp.write(TABLE_TOPIC)
+        for k, v in words_stat.items():
+            fp.write(f"{k:30} ==> {v:10d}\n")
+    else:
+        print(TABLE_TOPIC)
+        for k, v in words_stat.items():
+            print(f"{k:30} ==> {v:10d}\n")
+
+
+def average_words_amount(words_amount: list,  fp=None):
+    s = f"Average words amount in one sentence is {round(sum(words_amount) / len(words_amount), 1)}.\n"
+
+    if fp:
+        fp.write(s)
+    else:
+        print(s)
+
+
+def median_word_count(words_count, fp=None):
+    assert type(words_count) == list
+
+    words_count.sort()
+    num = (words_count[len(words_count) // 2] + words_count[(len(words_count) - 1) // 2]) / 2
+    s = f"Median word amount in one sentence is {num}.\n"
+    if fp:
+        fp.write(s)
+    else:
+        print(s)
 
 
 def solve(text=TEXT_EXAMPLE):
-    print("Processing text:")
-    print(text)
+    if FILE_OUTPUT:
+        fp = open(OUTPUT_FILE_NAME, "w")
+        fp.write(OUTPUT_TOPIC)
+        process_text(text, fp)
+    else:
+        print(OUTPUT_TOPIC)
+        print(text)
+        process_text(text)
 
 
